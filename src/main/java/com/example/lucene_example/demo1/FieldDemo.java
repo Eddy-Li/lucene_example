fb938ceed528c69610a622c08f3e861d9101ff12
Eddy-Li <liyanan2010@sina.cn>
2019-06-04 20:47:25 +0800

    111

--- src/main/java/com/example/lucene_example/demo1/FieldDemo.java
+++ src/main/java/com/example/lucene_example/demo1/FieldDemo.java
@@ -3,6 +3,7 @@ package com.example.lucene_example.demo1;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.PointValues;
+import org.apache.lucene.search.Query;
 
 public class FieldDemo {
     public static void main(String[] args) {
@@ -22,6 +23,7 @@ public class FieldDemo {
         //field.setStringValue("");
         //field.setDoubleValue(1.1);
 
+
         //一般使用Field的子类
         //字符串：创建索引、分词、存储
         TextField textField1 = new TextField("name", "value", Field.Store.YES);
@@ -39,28 +41,30 @@ public class FieldDemo {
         StoredField storedField4 = new StoredField("name", "1");
         StoredField storedField5 = new StoredField("name", "1".getBytes());
 
+        //只索引、不分词、不存储
+        //如果要存储，需要额外创建 StoredField storedField1 = new StoredField("name", xxx);
+        //IntPoint DoublePoint FloatPoint BigIntegerPoint同LongPoint
+        LongPoint longPoint1 = new LongPoint("name", 100);//1维数据
+        //还可以用于2维数据、3维数据情况
+        LongPoint longPoint2 = new LongPoint("name", 100, 200);//2维数据
+        LongPoint longPoint3 = new LongPoint("name", 100, 200, 300);//3维数据
+        //LongPoint的查询方法
+        Query query1 = LongPoint.newExactQuery("name", 100);//查询值等于100的字段
+        Query query2 = LongPoint.newSetQuery("name", 100, 200, 300);//查询值等于100、200、300的字段,还有一个重载为集合的方法，两个一样
+        Query query3 = LongPoint.newRangeQuery("name", 100, 200);//查询值在[100,200]的字段
+        Query query4 = LongPoint.newRangeQuery("name", Long.MIN_VALUE, 200);//查询值在(-∞,200]的字段
+        Query query5 = LongPoint.newRangeQuery("name", 100, Long.MAX_VALUE);//查询值在[100,+∞)的字段
+        Query query6 = LongPoint.newRangeQuery("name", 100, Math.addExact(200, 1));//查询值在[100,200)的字段
+        Query query7 = LongPoint.newRangeQuery("name", Math.addExact(100, -1), 200);//查询值在(100,200]的字段
+        Query query8 = LongPoint.newRangeQuery("name", new long[]{1, 2, 3}, new long[]{7, 8, 9});//多维数据时，x在[1,7],y在[2,8],z在[3,9]
 
-        /**
-         * An indexed {@code long} field for fast range filters.  If you also
-         * need to store the value, you should add a separate {@link StoredField} instance.
-         * <p>
-         * Finding all documents within an N-dimensional shape or range at search time is
-         * efficient.  Multiple values for the same field in one document
-         * is allowed.
-         * <p>
-         * This field defines static factory methods for creating common queries:
-         * <ul>
-         *   <li>{@link #newExactQuery(String, long)} for matching an exact 1D point.
-         *   <li>{@link #newSetQuery(String, long...)} for matching a set of 1D values.
-         *   <li>{@link #newRangeQuery(String, long, long)} for matching a 1D range.
-         *   <li>{@link #newRangeQuery(String, long[], long[])} for matching points/ranges in n-dimensional space.
-         * </ul>
-         * @see PointValues
-         */
-        LongPoint longPoint = new LongPoint();
-        IntPoint intPoint = new IntPoint();
-        DoublePoint doublePoint = new DoublePoint();
-        FloatPoint floatPoint = new FloatPoint();
-        BigIntegerPoint bigIntegerPoint = new BigIntegerPoint();
+
+        //LongRange用于（最大值、最小值）或者多对（最大值、最小值）的索引，以及查询方法，还有IntRange DoubleRange...
+        //LongRange longRange = new LongRange(String name, final long[] min, final long[] max)
+        //LongRange.newContainsQuery(String field, final long[] min, final long[] max) //包含
+        //LongRange.newCrossesQuery(String field, final long[] min, final long[] max) //相交
+        //LongRange.newWithinQuery(String field, final long[] min, final long[] max) //包含
+        //LongRange.newIntersectsQuery(String field, final long[] min, final long[] max) //相交
+
     }
 }
