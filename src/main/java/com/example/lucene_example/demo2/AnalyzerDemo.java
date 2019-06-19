2ceed92a6593029679aedec1001e2666b564ad61
Eddy-Li <liyanan2010@sina.cn>
2019-06-19 23:18:47 +0800

    111

---
+++ src/main/java/com/example/lucene_example/demo2/AnalyzerDemo.java
@@ -0,0 +1,41 @@
+package com.example.lucene_example.demo2;
+
+import com.example.lucene_example.ikAdapter.IKAnalyzer6x;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class AnalyzerDemo {
+
+
+    @Test
+    public void test1() throws IOException {
+        String str = "This is my house,I'm coming from JiangSu";
+        StandardAnalyzer standardAnalyzer = new StandardAnalyzer();
+        displayToken(str,standardAnalyzer);
+        SimpleAnalyzer simpleAnalyzer = new SimpleAnalyzer();
+        displayToken(str,simpleAnalyzer);
+        IKAnalyzer6x analyzer6x = new IKAnalyzer6x(true);
+        displayToken(str,analyzer6x);
+    }
+
+    private void displayToken(String content, Analyzer analyzer) throws IOException {
+        TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(content));
+        tokenStream.reset();
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        while (tokenStream.incrementToken()) {
+            System.out.print("[" + charTermAttribute + "]");
+        }
+        System.out.println("\n");
+        tokenStream.close();
+    }
+
+
+}
+
