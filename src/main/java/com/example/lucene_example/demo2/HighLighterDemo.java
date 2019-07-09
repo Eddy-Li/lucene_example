4436d34080a74e025dd630ae06acc1b8c0438931
Eddy-Li <liyanan2010@sina.cn>
2019-07-09 11:19:51 +0800

    111

--- src/main/java/com/example/lucene_example/demo2/HighLighterDemo.java
+++ src/main/java/com/example/lucene_example/demo2/HighLighterDemo.java
@@ -43,6 +43,9 @@ public class HighLighterDemo {
         Document doc2 = new Document();
         doc2.add(new TextField("content", "书写好中华民族伟大复兴的“三农”新篇章", Field.Store.YES));
         indexWriter.addDocument(doc2);
+        Document doc3 = new Document();
+        doc3.add(new TextField("content", "在正定三年多的时间里，他住在办公室，吃在大食堂，和大家一起排队打饭，一起蹲在树下聊天。他在大街上临时摆桌子，听取群众意见；敞开县委、县政府机关大门，请背着粪筐的老农进来交谈；让县委干部走上街头搞随机问卷调查", Field.Store.YES));
+        indexWriter.addDocument(doc3);
 
         indexWriter.commit();
 
@@ -67,16 +70,22 @@ public class HighLighterDemo {
         QueryParser queryParser = new QueryParser(defaultField, analyzer);
 
         //如果这里没有指定搜索字段(搜索字段:关键词，例如content:scala)，则会使用默认搜索字段即上面的defaultField
-        String queryStr = "复兴";
+        String queryStr = "群众意见";
         Query query = queryParser.parse(queryStr);
         TopDocs topDocs = indexSearcher.search(query, 10);
         System.out.println("总命中数:" + topDocs.totalHits);
         ScoreDoc[] scoreDocs = topDocs.scoreDocs;//命中的记录数组
 
-        SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter("<b style=\"color: red\">","</b>");
+        SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter("<b style=\"color: red\">", "</b>");
         QueryScorer scorer = new QueryScorer(query);
         Highlighter highlighter = new Highlighter(htmlFormatter, scorer);
-        highlighter.setTextFragmenter(new NullFragmenter());
+        /*
+         * 创建Fragmenter 作用是将原始字符串拆分成独立的片段
+         * SimpleSpanFragmenter 是尝试将让片段永远包含跨度匹配的文档
+         * SimpleFragmenter 是负责将文本拆分封固定字符长度的片段，但它并处理子边界（默认100）
+         * NullFragmenter 整个字符串作为单个片段返回，这适合于处理title域和前台文本较短的域
+         */
+        highlighter.setTextFragmenter(new SimpleSpanFragmenter(scorer, 40));
 
         for (ScoreDoc scoreDoc : scoreDocs) {
             int docId = scoreDoc.doc;//命中的文档id
